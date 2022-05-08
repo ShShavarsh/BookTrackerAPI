fbbf89394c54726143b28c26e17a5631e5e5eab4
Shavarsh Shahoyan <shavarsh.sh@gmail.com>
2022-05-08 17:14:27 +0400

    Adding BookServiceImpl class with stub methods

---
+++ src/main/java/com/example/BookTrackerAPI/services/BookServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.BookTrackerAPI.services;
+
+import org.springframework.stereotype.Service;
+
+import com.example.BookTrackerAPI.model.Book;
+import com.example.BookTrackerAPI.repositories.BookRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+	BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public List<Book> getBooks() {
+        return null;
+    }
+
+    @Override
+    public Book getBookById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Book insert(Book book) {
+    	 return null;
+    }
+
+    @Override
+    public void updateBook(Long id, Book book) {
+    	
+    }
+
+    @Override
+    public void deleteBook(Long bookId) {
+    	
+    }
+}
